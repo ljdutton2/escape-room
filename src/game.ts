ba545ed79b2a9ac227dbed2ab38b8e72bcac0049
lauren <ljdutton2@icloud.com>
2021-03-21 12:27:33 -0500

    animate door

--- src/game.ts
+++ src/game.ts
@@ -1,10 +1,5 @@
-// Create an entity for the main scene model
 const baseScene = new Entity();
-
-// Add it to the engine for rendering
 engine.addEntity(baseScene);
-
-// Give it a component for the model itself
 baseScene.addComponent(new GLTFShape("models/scene.glb"));
 
 // Add an entity for the door
@@ -14,3 +9,32 @@ engine.addEntity(door);
 // Give it a model and move it into place
 door.addComponent(new GLTFShape("models/room1/Puzzle01_Door.glb"));
 door.addComponent(new Transform({ position: new Vector3(21.18, 10.8, 24.5) }));
+
+// Add an Animator to play clips inside the model file, created by the artist
+door.addComponent(new Animator());
+// This model has an "Open" animation that when played should happen once and then stop moving
+door
+  .getComponent(Animator)
+  .addClip(new AnimationState("Door_Open", { looping: false }));
+
+// Add an AudioSource to play a squeak as the door opens
+door.addComponent(new AudioSource(new AudioClip("sounds/door_squeak.mp3")));
+
+// When the player clicks on the door, open it!
+let isDoorOpen = false;
+door.addComponent(
+  new OnClick((): void => {
+    // Track if the door has already been opened so we don't play the animation twice
+    if (!isDoorOpen) {
+      isDoorOpen = true;
+
+      // Play the animation
+      door
+        .getComponent(Animator)
+        .getClip("Door_Open")
+        .play();
+      // And the sound effect
+      door.getComponent(AudioSource).playOnce();
+    }
+  })
+);
