594949b1dd707a82f280382e7028791edcacf943
lauren <ljdutton2@icloud.com>
2021-03-21 11:15:35 -0500

    add interactive component

--- src/game.ts
+++ src/game.ts
@@ -6,3 +6,11 @@ engine.addEntity(baseScene);
 
 // Give it a component for the model itself
 baseScene.addComponent(new GLTFShape("models/scene.glb"));
+
+// Add an entity for the door
+const door = new Entity();
+engine.addEntity(door);
+
+// Give it a model and move it into place
+door.addComponent(new GLTFShape("models/room1/Puzzle01_Door.glb"));
+door.addComponent(new Transform({ position: new Vector3(21.18, 10.8, 24.5) }));
